1ad00a834a0c5e7eb8732959b20e5e9c448f8214
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-10-21 16:52:51 +0300

    Fix

--- aux_error1.c
+++ aux_error1.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcat_cd - function that concatenates the message for cd error
+ * strcat_cd - a  function that concatenates the message for cd error
  *
  * @datash: data relevant (directory)
  * @msg: message to print
